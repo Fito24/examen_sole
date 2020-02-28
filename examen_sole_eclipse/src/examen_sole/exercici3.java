edcc85ce98c5c8fa549c082f2bef118a50132a35
Fito24 <59867791+Fito24@users.noreply.github.com>
2020-02-28 09:30:26 +0100

    Add files via upload

---
+++ examen_sole_eclipse/src/examen_sole/exercici3.java
@@ -0,0 +1,18 @@
+package examen_sole;
+
+public class exercici3 {
+
+	public static void main(String[] args) {
+		printOne();
+		printOne();
+		printTwo();
+	}
+	public static void printOne() {
+		System.out.println("Hello World");
+	}
+	public static void printTwo() {
+		printOne();
+		printOne();
+	}
+
+}
